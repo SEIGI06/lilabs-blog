093684b5b43fa8c8cdedf61d9276aa98c5549582
SEIGI06 <lpeyr.ledantec@gmail.com>
2025-11-29 14:03:39 +0100

    refactor: update `@tailwindcss/typography` plugin import to ES module syntax.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
     content: [
@@ -49,8 +50,7 @@ const config: Config = {
         },
     },
     plugins: [
-        require('@tailwindcss/typography'),
+        typography,
     ],
 };
 export default config;
-
